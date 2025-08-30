424282a0ab2a0c68ee74f83d3a2462ffa2626967
Justin Iaconis <JIaconis@my.gcu.edu>
2025-08-29 17:15:35 -0700

    Initial commit

---
+++ src/test/java/com/gcu/cst339_group5/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gcu.cst339_group5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
