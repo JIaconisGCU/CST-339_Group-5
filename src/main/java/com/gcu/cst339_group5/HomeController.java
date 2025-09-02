7a55193aac239bf079b1785602b89ee8e5bb4a04
CarlosECortes <shocktroopcortes@gmail.com>
2025-09-01 20:51:57 -0700

    Updated homepage theme, navbar styling, and footer fixes

--- src/main/java/com/gcu/cst339_group5/HomeController.java
+++ src/main/java/com/gcu/cst339_group5/HomeController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    // Map HTTP GET requests for the root path "/" to this method.
+	// This means when a user visits http://localhost:8080/, this method will execute.
   @GetMapping("/")
   public String home(Model model) {
+	  
+	 // "pageTitle" is the key, "Video Game Library" is the value.
+	 // This allows the Thymeleaf template (home.html) to access ${pageTitle}.
     model.addAttribute("pageTitle", "Video Game Library");
     return "home"; // resolves to templates/home.html
   }
